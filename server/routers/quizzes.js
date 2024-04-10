6d3669d600ddd6df5b105a6dc3d3ead40988a02e
AD202310 <a.g.dychenko@gmail.com>
2024-04-10 19:07:37 -0400

    Reformat router

--- server/routers/quizzes.js
+++ server/routers/quizzes.js
@@ -5,16 +5,11 @@ const { getAllQuizzes } = require('../db/queries/quizzes');
 router.get('/', async (req, res) => {
   try {
     const quizzes = await getAllQuizzes();
-    res.status(200).json({
-      message: "Successfully retrieved all quizzes",
-      data: quizzes
-    });
+    console.log(quizzes);  // Console log
+    res.json(quizzes); // Send as a JSON response
   } catch (error) {
-    console.error('Error fetching quizzes:', error);
-    res.status(500).json({
-      message: "Error fetching quizzes",
-      error: error.message
-    });
+    console.error(error);
+    res.status(500).send('An error occurred while retrieving the quizzes.'); // Send an error response
   }
 });
 
